bd49cbc8eee4b1562e5f6053127ebbd87d279e0e
Rilwanu Idris <133426348+rillygulma@users.noreply.github.com>
2024-06-11 22:59:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
-    strictPort: true, // Fail if the port is already in use
-    host: '0.0.0.0' // Listen on all addresses, needed for Render
-  }
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 });
